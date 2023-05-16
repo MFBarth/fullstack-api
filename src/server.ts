69d1df6d14da282c5fe7d1b3aebd24de98aeeabd
Matheus Barth <matheusbarthf@gmail.com>
2023-05-16 19:48:19 -0300

    create swagger

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,13 @@ import { env } from '@/env';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticate';
 import userRoutes from './controllers/user/user.route';
 import sessionRoutes from './controllers/session/session.route';
+import swaggerDocs from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/session', sessionRoutes);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api/user', ensureAuthenticated, userRoutes);
 app.use(express.static(__dirname + '/public'));
 
