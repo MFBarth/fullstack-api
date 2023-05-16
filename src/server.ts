ddf0f441a9421f9feec0e876b13d75c18a93f293
Matheus Barth <matheusbarthf@gmail.com>
2023-05-15 22:00:05 -0300

    feat: create static pages

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 
 import { env } from '@/env';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticate';
 import userRoutes from './controllers/user/user.route';
 
 const app = express();
 
 app.use(express.json());
-app.use('/user', userRoutes);
+app.use('/api/user', ensureAuthenticated, userRoutes);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/login', (_, res) => {
+  res.sendFile(__dirname + '/public/html/login.html');
+});
+
+app.get('/', ensureAuthenticated, (_, res) => {
+  res.sendFile(__dirname + '/public/html/home.html');
+});
 
 app.listen(env.API_PORT, () => {
   console.log(`▶️ Server started on port ${env.API_PORT}!`);
